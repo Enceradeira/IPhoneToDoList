397e32694ce4ee4224858a4cfe5f97abd0d3cc28
Jorg <Jorg@JENNIUS-WS4.local>
2014-05-30 10:30:16 +0100

    Initial Commit

---
+++ ToDoList/JENAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JENAppDelegate.h
+//  ToDoList
+//
+//  Created by Jorg on 30/05/2014.
+//  Copyright (c) 2014 co.uk.jennius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JENAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Jorg on 30/05/2014.
+//  Copyright (c) 2014 co.uk.jennius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JENAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JENAppDelegate class]));
+    }
+}
