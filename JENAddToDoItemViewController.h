43a420be704c0c80b72e86608f2f667320742069
Jorg Jenni <jorg.jenni@jennius.co.uk>
2014-06-02 13:38:30 +0100

    generic controllers with specific ones replaced

---
+++ JENAddToDoItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  JENAddToDoItemViewController.h
+//  ToDoList
+//
+//  Created by Jorg on 02/06/2014.
+//  Copyright (c) 2014 co.uk.jennius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JENAddToDoItemViewController : UIViewController
+
+@end
---
+++ ToDoList/JENToDoListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JENToDoListTableViewController.h
+//  ToDoList
+//
+//  Created by Jorg on 02/06/2014.
+//  Copyright (c) 2014 co.uk.jennius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JENToDoListTableViewController : UITableViewController
+
+@end
